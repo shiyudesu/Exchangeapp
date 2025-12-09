9f6889b244a4593514956c8c6d08a4646ac9291c
shiyu <ming051017@outlook.com>
2025-12-09 16:58:20 +0800

    login

--- Exchangeapp_backend/router/router.go
+++ Exchangeapp_backend/router/router.go
@@ -1,7 +1,7 @@
 package router
 
 import (
-	"net/http"
+	"exchangeapp/Exchangeapp/Exchangeapp_backend/controllers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,16 +11,8 @@ func SetupRouter() *gin.Engine {
 
 	auth := r.Group("/api/auth")
 	{
-		auth.POST("/login", func(ctx *gin.Context) {
-			ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"msg": "Login Success",
-			})
-		})
-		auth.POST("/register", func(ctx *gin.Context) {
-			ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"msg": "Register Success",
-			})
-		})
+		auth.POST("/login", controllers.Login)
+		auth.POST("/register", controllers.Register)
 	}
 	return r
 }
--- Exchangeapp_backend/utils/utils.go
+++ Exchangeapp_backend/utils/utils.go
@@ -21,3 +21,8 @@ func GenerateJWT(username string) (string, error) {
 	signedToken, err := token.SignedString([]byte("secret"))
 	return "Bearer " + signedToken, err
 }
+
+func CheckPassword(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
